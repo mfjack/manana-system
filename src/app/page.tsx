de17345ec5d710cb70be93eccea3c8b0a887f4ff
Jack <ferreira.marlon@live.com>
2025-10-12 18:57:35 -0300

    feat: add orders page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
-import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
-export default function Home() {
+type status = "Livre" | "Ocupado";
+
+type Order = {
+  id: number;
+  status: status;
+};
+
+const orders: Order[] = [
+  { id: 1, status: "Livre" },
+  { id: 2, status: "Ocupado" },
+];
+
+export default function OrderPage() {
   return (
-    <main>
-      <Button>Click me</Button>
-    </main>
+    <section className="h-screen p-6 flex flex-col overflow-y-auto">
+      <h1>Comandas</h1>
+
+      <div className="flex flex-col mt-8 gap-6 items-center sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {orders.map((order) => (
+          <Card
+            key={order.id}
+            className={cn(
+              "h-20 justify-center hover:transition-transform hover:scale-[1.01] cursor-pointer w-full rounded-sm shadow lg:h-24",
+              order.status === "Ocupado" ? "bg-red-50" : "bg-emerald-50"
+            )}
+          >
+            <CardContent className="flex justify-between">
+              <h2 className="text-xl font-medium">{order.id}</h2>
+              <Badge className={cn("text-md rounded-sm", order.status === "Ocupado" ? "bg-red-500" : "bg-emerald-500")}>
+                {order.status}
+              </Badge>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </section>
   );
 }
